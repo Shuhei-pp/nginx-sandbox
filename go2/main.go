c533ef1aed93668a57b8fba367e060792c88e41d
Shuhei-pp <lotakotayota@gmail.com>
2024-11-19 21:00:49 +0900

    add

--- go2/main.go
+++ go2/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// 1秒待つ
+		time.Sleep(1 * time.Second)
 		w.Write([]byte("server2です"))
 	})
 
--- go3/main.go
+++ go3/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// 10秒待つ
+		time.Sleep(10 * time.Second)
 		w.Write([]byte("server3です"))
 	})
 
